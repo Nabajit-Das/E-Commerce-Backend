7f3ab2670cc8a6a9ca94593e41c78532d9c363b2
Nabajit Das <dasnabajit2005@gmail.com>
2024-04-20 15:15:44 +0530

    "Mongo DB atlas"

--- configs/db.config.js
+++ configs/db.config.js
@@ -4,5 +4,5 @@
 
 module.exports={
     DB_NAME:"e-comm",
-    DB_URL:"mongodb://localhost/e-comm"
+    DB_URL:"mongodb+srv://dasnabajit2005:nabajit@cluster0.uqepldr.mongodb.net/e-comm?retryWrites=true&w=majority&appName=Cluster0"
 }
